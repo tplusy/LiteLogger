285d07f2ab3be6de0e0e7283bf9040f443d2bdc5
helge.wang <helge.wang@gfanxvision.com>
2024-01-13 14:12:29 +0800

    LiteLogger based on LiteLogger

---
+++ build/CMakeFiles/LiteLogger.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LiteLogger.dir/src/elog.c.o"
+  "CMakeFiles/LiteLogger.dir/src/elog.c.o.d"
+  "CMakeFiles/LiteLogger.dir/src/elog_async.c.o"
+  "CMakeFiles/LiteLogger.dir/src/elog_async.c.o.d"
+  "CMakeFiles/LiteLogger.dir/src/elog_buf.c.o"
+  "CMakeFiles/LiteLogger.dir/src/elog_buf.c.o.d"
+  "CMakeFiles/LiteLogger.dir/src/elog_file.c.o"
+  "CMakeFiles/LiteLogger.dir/src/elog_file.c.o.d"
+  "CMakeFiles/LiteLogger.dir/src/elog_file_port.c.o"
+  "CMakeFiles/LiteLogger.dir/src/elog_file_port.c.o.d"
+  "CMakeFiles/LiteLogger.dir/src/elog_port.c.o"
+  "CMakeFiles/LiteLogger.dir/src/elog_port.c.o.d"
+  "CMakeFiles/LiteLogger.dir/src/elog_utils.c.o"
+  "CMakeFiles/LiteLogger.dir/src/elog_utils.c.o.d"
+  "libLiteLogger.a"
+  "libLiteLogger.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/LiteLogger.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/LiteLogger.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for LiteLogger.
+# This may be replaced when dependencies are built.
